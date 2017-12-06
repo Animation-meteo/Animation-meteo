36382a8b7f15de665dd51fa98e84de34400a3dad
bossertsebastien <seb298600@gmail.com>
2017-12-06 09:27:59 +0100

    tt

---
+++ src/Test/Testgetgribbymail.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Testgetgribbymail {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
